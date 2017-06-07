be486680a7d049bcc4c2fd833df54931470ac41d
Doug Black <dblack@americanbible.org>
2017-06-07 13:35:00 -0400

    Working div update with count

--- theme/js/uvdv.js
+++ theme/js/uvdv.js
@@ -8,10 +8,22 @@ function upvotedownvote(url, ud) {
 		success: function(rdata){
 			console.log(rdata);
 			// Get count div
+			var count = $('.upvotedownvote-block .count').text();
 			// Get total div
+			var total = $('.upvotedownvote-block .mini').text();
+			total = total.replace( /^\D+/g, '').split(' ');
+			total = parseInt(total);
 			// Add one to each
+			total++;
+			if (ud === 'up') {
+				count++;
+			} else {
+				count--;
+			}
 			// Update the divs
-			},
+			$('.upvotedownvote-block .count').text(count);
+			$('.upvotedownvote-block .mini').text('('+total+' votes)');
+		},
 		error: function(xhr){
 			console.log(xhr);
 		}
